63c11f548510b281d67186059b0b50f106667e6a
octavian.fermus <octavian.fermus@gmail.com>
2016-01-26 18:55:01 +0200

    started turning to SCSS from CSS in front-dev for easier stylesheet modification
    (Need to install ruby and sass for front development, then run npm install again)

--- front-dev/Gruntfile.js
+++ front-dev/Gruntfile.js
@@ -1,37 +1,48 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
   // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    jade: {
-        compile: {
-            options: {
-                data: {
-                    debug: false
-                }
-            },
-            files: [{
-                src: "*.jade",
-                dest: "../public/",
-                ext: ".html",
-                expand: true,
-                cwd: "jade/"
-            }]
-        }
-    },
-    watch: {
-      scripts: {
-        files: ['jade/*.jade', 'jade/**/*.jade', 'jade/**/**/*.jade'],
-        tasks: ['jade'],
-        options: {
-          spawn: false,
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        jade: {
+            compile: {
+                options: {
+                    data: {
+                        debug: false
+                    }
+                },
+                files: [{
+                    src: "*.jade",
+                    dest: "../public/",
+                    ext: ".html",
+                    expand: true,
+                    cwd: "jade/"
+                }]
+            }
         },
-      },
-    },
-  });
-  grunt.loadNpmTasks('grunt-contrib-jade');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  // Default task(s).
-  grunt.registerTask('default', ['jade','watch']);
+        sass: {
+            dist: {
+                options: {
+                    style: 'expanded'
+                },
+                files: {
+                    '../public/css/stylesheet.css': 'scss/stylesheet.scss'
+                }
+            }
+        },
+        watch: {
+            scripts: {
+                files: ['jade/*.jade', 'jade/**/*.jade', 'jade/**/**/*.jade', 'scss/*.scss'],
+                tasks: ['jade','sass'],
+                options: {
+                    spawn: false
+                }
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-jade');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
+    // Default task(s).
+    grunt.registerTask('default', ['jade', 'sass', 'watch']);
 };
