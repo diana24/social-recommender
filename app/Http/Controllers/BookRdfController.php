22714c3a98644e6174523f6847a6c3d55fdc8566
diana24 <dyana_blue_24@yahoo.com>
2016-01-27 23:56:50 +0200

    book recommendations done

--- app/Http/Controllers/BookRdfController.php
+++ app/Http/Controllers/BookRdfController.php
@@ -10,73 +10,11 @@ use EasyRdf_Graph;
 use EasyRdf_Namespace;
 use EasyRdf_Sparql_Client;
 use Illuminate\Support\Facades\Auth;
+use Mockery\CountValidator\Exception;
 
 class BookRdfController extends Controller
 {
-    function searchBooks(Request $request=null){
-        (new RdfController())->initRdf();
-        if(!$request){
-            return json_encode([]);
-        }
-        $authorUri = $request->get('authorUri');
-        $illustratorUri = $request->get('illustratorUri');
-//        $releaseDate= $request->get('releaseDate');
-        $literaryGenreUri= $request->get('literaryGenreUri');
-        $name = $request->get('name');
-        $numberOfPagesMin = $request->get('numberOfPagesMin');
-        $numberOfPagesMax = $request->get('numberOfPagesMax');
-        $numberOfVolumes = $request->get('numberOfVolumes');
-
-        $sparql = new EasyRdf_Sparql_Client('http://dbpedia.org/sparql');
-
-        $query='select *
-                where {
-                { ?book rdf:type dbo:WrittenWork }
-                 union
-                { ?book rdf:type <http://dbpedia.org/class/Book> }
-                  union
-                { ?book rdf:type owl:Thing }.
-                ?book rdfs:label ?label.
-                optional { {?book dbo:author ?author} union {?book dbp:author ?author} }.
-                optional { {?book dbo:illustrator ?illustrator} union {?book dbp:illustrator ?illustrator}}.
-                optional { {?book dbo:genre ?genre} union {?book dbp:genre ?genre}}.
-                optional { {?book dbp:imageCaption ?image} union {?book dbo:imageCaption ?image} union {?book foaf:depiction ?image} }.
-                optional { {?book dbo:numberOfPages ?numberOfPages} union {?book dbp:numberOfPages ?numberOfPages} }.
-                optional { {?book dbo:numberOfVolumes ?numberOfVolumes} union {?book dbp:numberOfVolumes ?numberOfVolumes} }.
-                optional { {?book dbp:releaseDate ?releaseDate} union {?book dbo:releaseDate ?releaseDate} }.';
-        if(isset($authorUri)){
-            $query .= "\n".' {?book dbo:author <'.$authorUri.'>} union {?book dbp:author <'.$authorUri.'>} .';
-        }
-        if(isset($illustratorUri)){
-            $query .= "\n".'{?book dbo:illustrator <'.$illustratorUri.'>} union {?book dbp:illustrator <'.$illustratorUri.'>} .';
-        }
-        if(isset($literaryGenreUri)){
-            $query .= "\n".'{?book dbo:genre <'.$literaryGenreUri.'>} union {?book dbp:genre <'.$literaryGenreUri.'>}.';
-        }
-        $query .= "\n".'filter ( lang(?label) = "en")';
-        if(isset($name) && strlen($name)){
-            $query .= "\n".'filter regex(str(?label), "'.$name.'"^^xsd:string, "i")';
-        }
-        if(isset($numberOfPagesMin)){
-            $query .= "\n".'filter (?numberOfPages >= '.$numberOfPagesMin.')';
-        }
-        if(isset($numberOfPagesMax)){
-            $query .= "\n".'filter (?numberOfPages <= '.$numberOfPagesMax.')';
-        }
-        if(isset($numberOfVolumes)){
-            $query .= "\n".'filter (?numberOfVolumes = '.$numberOfVolumes.')';
-        }
-//        if(isset($releaseDate)){
-//            $query .= "\n".'filter (?releaseDate = '.$releaseDate.')';
-//        }
-        $query .= '}  limit 50'; //dd($query);
-
-        try{
-            $result = $sparql->query($query);
-        } catch(\Exception $e){
-            return json_encode([]);
-        }
-
+    function unify($result, EasyRdf_Sparql_Client $sparql){
         $books=[];
         foreach($result as $row){
             $uri = $row->book->getUri();
@@ -156,6 +94,74 @@ class BookRdfController extends Controller
             }
             $books[$uri]=$book;
         }
+        return $books;
+    }
+
+    function searchBooks(Request $request=null){
+        (new RdfController())->initRdf();
+        if(!$request){
+            return json_encode([]);
+        }
+        $authorUri = $request->get('authorUri');
+        $illustratorUri = $request->get('illustratorUri');
+//        $releaseDate= $request->get('releaseDate');
+        $literaryGenreUri= $request->get('literaryGenreUri');
+        $name = $request->get('name');
+        $numberOfPagesMin = $request->get('numberOfPagesMin');
+        $numberOfPagesMax = $request->get('numberOfPagesMax');
+        $numberOfVolumes = $request->get('numberOfVolumes');
+
+        $sparql = new EasyRdf_Sparql_Client('http://dbpedia.org/sparql');
+
+        $query='select *
+                where {
+                { ?book rdf:type dbo:WrittenWork }
+                 union
+                { ?book rdf:type <http://dbpedia.org/class/Book> }
+                  union
+                { ?book rdf:type owl:Thing }.
+                ?book rdfs:label ?label.
+                optional { {?book dbo:author ?author} union {?book dbp:author ?author} }.
+                optional { {?book dbo:illustrator ?illustrator} union {?book dbp:illustrator ?illustrator}}.
+                optional { {?book dbo:genre ?genre} union {?book dbp:genre ?genre}}.
+                optional { {?book dbp:imageCaption ?image} union {?book dbo:imageCaption ?image} union {?book foaf:depiction ?image} }.
+                optional { {?book dbo:numberOfPages ?numberOfPages} union {?book dbp:numberOfPages ?numberOfPages} }.
+                optional { {?book dbo:numberOfVolumes ?numberOfVolumes} union {?book dbp:numberOfVolumes ?numberOfVolumes} }.
+                optional { {?book dbp:releaseDate ?releaseDate} union {?book dbo:releaseDate ?releaseDate} }.';
+        if(isset($authorUri)){
+            $query .= "\n".' {?book dbo:author <'.$authorUri.'>} union {?book dbp:author <'.$authorUri.'>} .';
+        }
+        if(isset($illustratorUri)){
+            $query .= "\n".'{?book dbo:illustrator <'.$illustratorUri.'>} union {?book dbp:illustrator <'.$illustratorUri.'>} .';
+        }
+        if(isset($literaryGenreUri)){
+            $query .= "\n".'{?book dbo:genre <'.$literaryGenreUri.'>} union {?book dbp:genre <'.$literaryGenreUri.'>}.';
+        }
+        $query .= "\n".'filter ( lang(?label) = "en")';
+        if(isset($name) && strlen($name)){
+            $query .= "\n".'filter regex(str(?label), "'.$name.'"^^xsd:string, "i")';
+        }
+        if(isset($numberOfPagesMin)){
+            $query .= "\n".'filter (?numberOfPages >= '.$numberOfPagesMin.')';
+        }
+        if(isset($numberOfPagesMax)){
+            $query .= "\n".'filter (?numberOfPages <= '.$numberOfPagesMax.')';
+        }
+        if(isset($numberOfVolumes)){
+            $query .= "\n".'filter (?numberOfVolumes = '.$numberOfVolumes.')';
+        }
+//        if(isset($releaseDate)){
+//            $query .= "\n".'filter (?releaseDate = '.$releaseDate.')';
+//        }
+        $query .= '}  limit 200'; //dd($query);
+
+        try{
+            $result = $sparql->query($query);
+        } catch(\Exception $e){
+            return json_encode([]);
+        }
+
+        $books=$this->unify($result,$sparql);
 //        dd($books);
         return json_encode($books);
     }
@@ -167,17 +173,16 @@ class BookRdfController extends Controller
 
         shuffle($mybooks);
 
-        $n=10;
+        $n=5;
         while($n>count($mybooks)){
             $n--;
         }
-
+        $results=[];
+        $sparql = new EasyRdf_Sparql_Client('http://dbpedia.org/sparql');
         $mybooks = array_slice($mybooks, 0, $n);
-
         foreach($mybooks as $mb){
             try{
-                $sparql = new EasyRdf_Sparql_Client('http://dbpedia.org/sparql');
-                $query = 'select distinct ?book where {
+                $query = 'select * where {
 
                 { ?book rdf:type dbo:WrittenWork }
                  union
@@ -192,31 +197,83 @@ class BookRdfController extends Controller
                 {?book dbo:publisher ?publisher} union {?book dbp:publisher ?publisher}
                 ?publisher rdfs:label ?publisherName
 
-                {?book dbo:genre ?gen} union {?book dbp:genre ?gen}.
-                ?gen rdfs:label ?genLabel.
+                {?book dbo:genre ?genre} union {?book dbp:genre ?genre}.
+                ?genre rdfs:label ?genLabel.
+                optional { {?book dbp:imageCaption ?image} union {?book dbo:imageCaption ?image} union {?book foaf:depiction ?image} }.
 
-                filter ( lang(?label) = "en" )
-                {filter regex(str(?genLabel), "' . $mb['genre'] . '"^^xsd:string, "i")}';
+                filter ( lang(?label) = "en" && lang(?authorName) = "en" && lang(?publisherName) = "en" && lang(?genLabel) = "en")';
+                if(isset($mb['genre'])){
+                    $query .= 'optional {filter regex(str(?genLabel), "' . $mb['genre'] . '"^^xsd:string, "i")}';
+                }
                 foreach($mb['authors'] as $author){
-                    $query .= ' union {filter regex(str(?authorName), "' . $author . '"^^xsd:string, "i")}';
+                    $query .= ' optional {filter regex(str(?authorName), "' . $author . '"^^xsd:string, "i")}';
                 }
                 foreach($mb['publishers'] as $publisher){
-                    $query .= ' union {filter regex(str(?publisherName), "' . $publisher . '"^^xsd:string, "i")}';
+                    $query .= ' optional {filter regex(str(?publisherName), "' . $publisher . '"^^xsd:string, "i")}';
                 }
                 $query .= '} limit 10';
 
-                $result = $sparql->query($query);
-                foreach($result as $row){
-                    $uri = $row->book->getUri();
-                    $book = $this->getBookData($uri);
-                    array_push($books,$book);
+                  $result = $sparql->query($query);
+                array_push($results,$result);
+
+
+            }catch (\Exception $e){
+//                dd($e);
+            }
+    }
+//        dd($results);
+        foreach($results as $result){
+            foreach($result as $row){
+                $uri = $row->book->getUri();
+                if(!array_has($books,$uri)){
+                    $books[$uri]=[];
                 }
-            }
-            catch(\Exception $e){
+                $book=$books[$uri];
+                $book['title']=$row->label->getValue();
+                if(isset($row->author) && method_exists($row->author, 'getUri')){
+                    $authorUri = $row->author->getUri();
+                    if(!isset($book['authors'])){
+                        $book['authors']=[];
+                    }
+                    if(!array_has($book['authors'],$authorUri)){
+                        $authorUri = $row->author->getUri();
+                        $query = 'select ?authorName where { <'.$authorUri.'> rdfs:label ?authorName . filter (lang(?authorName)="en")} limit 1';
+                        $r = $sparql->query($query);
+                        foreach($r as $rw){
+                            if(isset($rw->authorName)){
+                                $authorName = $rw->authorName->getValue();
+                                $book['authors'][$authorUri]=$authorName;
+                            }
+                        }
+                    }
 
-            }
+                }
+                if(isset($row->genre) && method_exists($row->genre, 'getUri')){
+                    $genreUri = $row->genre->getUri();
+                    if(!isset($book['genres'])){
+                        $book['genres']=[];
+                    }
+                    if(!array_has($book['genres'],$genreUri)){
+                        $genreUri = $row->genre->getUri();
+                        $query = 'select ?genreName where { <'.$genreUri.'> rdfs:label ?genreName . filter (lang(?genreName)="en")} limit 1';
+                        $r = $sparql->query($query);
+                        foreach($r as $rw){
+                            if(isset($rw->genreName)){
+                                $genreName = $rw->genreName->getValue();
+                                $book['genres'][$genreUri]=$genreName;
+                            }
+                        }
+                    }
 
-        }
+                }
+                if(isset($row->image)){
+                    $book['image']=(method_exists($row->image, 'getUri')) ? $row->image->getUri() : (
+                    (method_exists($row->image, 'getValue')) ? $row->image->getValue() : $row->image
+                    );
+                }
+                $books[$uri]=$book;
+            }
+        }//dd($books);
         return json_encode($books);
     }
 }
