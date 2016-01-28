bd2fe05f0b4a5f536d383f793521c460dfac7d82
octavian.fermus <octavian.fermus@gmail.com>
2016-01-29 00:31:08 +0200

    small changes to css

--- public/js/userHeaderAjax.js
+++ public/js/userHeaderAjax.js
@@ -1,1 +1,1 @@
-$(document).on("ready",function(){jQuery.ajax({method:"get",url:"graph/me",dataType:"json",success:function(data){console.log("success"),console.log(data)},error:function(data){console.log("error")}})});
+$(document).on("ready",function(){jQuery.ajax({method:"get",url:"graph/me",dataType:"json",success:function(data){console.log("success"),console.log(data)},error:function(data){}})});
