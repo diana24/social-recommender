74740d6e9855f77c317dda4e09a9a786d380aa77
octavian.fermus <octavian.fermus@gmail.com>
2016-01-29 19:57:01 +0200

    multiple display changes

--- front-dev/js/searches.js
+++ front-dev/js/searches.js
@@ -7,8 +7,9 @@ $(document).on('ready', function () {
         illustrators = {},
         authors = {},
         institutionTypes = {},
+        professions = {},
         readyCheck = 0,
-        readyCount = 8;
+        readyCount = 9;
     $("p.resultHeader").html("Fetching initial data..");
     $(".allResults").html("");
     jQuery.ajax({
@@ -26,7 +27,7 @@ $(document).on('ready', function () {
                 source: autocompleteValues
             });
             readyCheck += 1;
-            $(".allResults").append("<p>Added autocomplete educational institution types</p>");
+            $(".allResults").append("<p class='log info'>Added autocomplete educational institution types</p>");
             if (readyCheck === readyCount) {
                 $(".loader").remove();
                 $("#accordion").removeClass("hidden");
@@ -36,7 +37,7 @@ $(document).on('ready', function () {
         },
         error: function () {
             readyCheck += 1;
-            $(".allResults").append("<p>Error: Autocomplete educational institution types failed.. disabling field..</p>");
+            $(".allResults").append("<p class='log error'>Error: Autocomplete educational institution types failed.. disabling field..</p>");
             $("input[name='eduTypeUri']").attr("disabled", "disabled");
             if (readyCheck === readyCount) {
                 $(".loader").remove();
@@ -64,7 +65,7 @@ $(document).on('ready', function () {
                 source: autocompleteValues
             });
             readyCheck += 1;
-            $(".allResults").append("<p>Added autocomplete locations</p>");
+            $(".allResults").append("<p class='log info'>Added autocomplete locations</p>");
             if (readyCheck === readyCount) {
                 $(".loader").remove();
                 $("#accordion").removeClass("hidden");
@@ -74,7 +75,7 @@ $(document).on('ready', function () {
         },
         error: function () {
             readyCheck += 1;
-            $(".allResults").append("<p>Error: Autocomplete locations failed.. disabling field..</p>");
+            $(".allResults").append("<p class='log error'>Error: Autocomplete locations failed.. disabling field..</p>");
             $("input[name='locationUri']").attr("disabled", "disabled");
             if (readyCheck === readyCount) {
                 $(".loader").remove();
@@ -99,7 +100,7 @@ $(document).on('ready', function () {
                 source: autocompleteValues
             });
             readyCheck += 1;
-            $(".allResults").append("<p>Added autocomplete event types</p>");
+            $(".allResults").append("<p class='log info'>Added autocomplete event types</p>");
             if (readyCheck === readyCount) {
                 $(".loader").remove();
                 $("#accordion").removeClass("hidden");
@@ -109,7 +110,7 @@ $(document).on('ready', function () {
         },
         error: function () {
             readyCheck += 1;
-            $(".allResults").append("<p>Error: Autocomplete event types failed.. disabling field..</p>");
+            $(".allResults").append("<p class='log error'>Error: Autocomplete event types failed.. disabling field..</p>");
             $("input[name='eventTypeUri']").attr("disabled", "disabled");
             if (readyCheck === readyCount) {
                 $(".loader").remove();
@@ -134,7 +135,7 @@ $(document).on('ready', function () {
                 source: autocompleteValues
             });
             readyCheck += 1;
-            $(".allResults").append("<p>Added autocomplete literary genres</p>");
+            $(".allResults").append("<p class='log info'>Added autocomplete literary genres</p>");
             if (readyCheck === readyCount) {
                 $(".loader").remove();
                 $("#accordion").removeClass("hidden");
@@ -144,7 +145,7 @@ $(document).on('ready', function () {
         },
         error: function () {
             readyCheck += 1;
-            $(".allResults").append("<p>Error: Autocomplete literary genres failed.. disabling field..</p>");
+            $(".allResults").append("<p class='log error'>Error: Autocomplete literary genres failed.. disabling field..</p>");
             $("input[name='literaryGenreUri']").attr("disabled", "disabled");
             if (readyCheck === readyCount) {
                 $(".loader").remove();
@@ -172,7 +173,7 @@ $(document).on('ready', function () {
                 source: autocompleteValues
             });
             readyCheck += 1;
-            $(".allResults").append("<p>Added autocomplete illustrators</p>");
+            $(".allResults").append("<p class='log info'>Added autocomplete illustrators</p>");
             if (readyCheck === readyCount) {
                 $(".loader").remove();
                 $("#accordion").removeClass("hidden");
@@ -182,7 +183,7 @@ $(document).on('ready', function () {
         },
         error: function () {
             readyCheck += 1;
-            $(".allResults").append("<p>Error: Autocomplete illustrators fetch failed.. disabling field..</p>");
+            $(".allResults").append("<p class='log error'>Error: Autocomplete illustrators fetch failed.. disabling field..</p>");
             $("input[name='illustratorUri']").attr("disabled", "disabled");
             if (readyCheck === readyCount) {
                 $(".loader").remove();
@@ -210,7 +211,7 @@ $(document).on('ready', function () {
                 source: autocompleteValues
             });
             readyCheck += 1;
-            $(".allResults").append("<p>Added autocomplete authors</p>");
+            $(".allResults").append("<p class='log info'>Added autocomplete authors</p>");
             if (readyCheck === readyCount) {
                 $(".loader").remove();
                 $("#accordion").removeClass("hidden");
@@ -220,7 +221,7 @@ $(document).on('ready', function () {
         },
         error: function () {
             readyCheck += 1;
-            $(".allResults").append("<p>Error: Autocomplete authors fetch failed.. disabling field..</p>");
+            $(".allResults").append("<p class='log error'>Error: Autocomplete authors fetch failed.. disabling field..</p>");
             $("input[name='authorUri']").attr("disabled", "disabled");
             if (readyCheck === readyCount) {
                 $(".loader").remove();
@@ -245,7 +246,7 @@ $(document).on('ready', function () {
                 source: autocompleteValues
             });
             readyCheck += 1;
-            $(".allResults").append("<p>Added autocomplete place types</p>");
+            $(".allResults").append("<p class='log info'>Added autocomplete place types</p>");
             if (readyCheck === readyCount) {
                 $(".loader").remove();
                 $("#accordion").removeClass("hidden");
@@ -255,7 +256,7 @@ $(document).on('ready', function () {
         },
         error: function () {
             readyCheck += 1;
-            $(".allResults").append("<p>Error: Autocomplete place types fetch failed.. disabling field..</p>");
+            $(".allResults").append("<p class='log error'>Error: Autocomplete place types fetch failed.. disabling field..</p>");
             $("input[name='placeTypeUri']").attr("disabled", "disabled");
             if (readyCheck === readyCount) {
                 $(".loader").remove();
@@ -279,8 +280,11 @@ $(document).on('ready', function () {
             $("input[name='countryUri']").autocomplete({
                 source: autocompleteValues
             });
+            $("input[name='personCountry']").autocomplete({
+                source: autocompleteValues
+            });
             readyCheck += 1;
-            $(".allResults").append("<p>Added autocomplete countries</p>");
+            $(".allResults").append("<p class='log info'>Added autocomplete countries</p>");
             if (readyCheck === readyCount) {
                 $(".loader").remove();
                 $("#accordion").removeClass("hidden");
@@ -291,7 +295,7 @@ $(document).on('ready', function () {
         },
         error: function () {
             readyCheck += 1;
-            $(".allResults").append("<p>Error: Autocomplete countries fetch failed.. disabling field..</p>");
+            $(".allResults").append("<p class='log error'>Error: Autocomplete countries fetch failed.. disabling field..</p>");
             $("input[name='countryUri']").attr("disabled", "disabled");
             if (readyCheck === readyCount) {
                 $(".loader").remove();
@@ -301,7 +305,102 @@ $(document).on('ready', function () {
             }
         }
     });
-    
+    jQuery.ajax({
+        method: 'get',
+        url: "/getProfessions",
+        dataType: "json",
+        success: function (data) {
+            var autocompleteValues = [],
+                key;
+            for (key in data) {
+                autocompleteValues.push(data[key].name);
+                professions[data[key].name.split(" ").join("_")] = data[key].uri;
+            }
+            $("input[name='personProfession']").autocomplete({
+                source: autocompleteValues
+            });
+            readyCheck += 1;
+            $(".allResults").append("<p class='log info'>Added autocomplete professions</p>");
+            if (readyCheck === readyCount) {
+                $(".loader").remove();
+                $("#accordion").removeClass("hidden");
+                $("p.resultHeader").html("Initial fetch complete!");
+                $(".allResults").append("You may now use the search bar!");
+            }
+            
+        },
+        error: function () {
+            readyCheck += 1;
+            $(".allResults").append("<p class='log error'>Error: Autocomplete professions fetch failed.. disabling field..</p>");
+            $("input[name='countryUri']").attr("disabled", "disabled");
+            if (readyCheck === readyCount) {
+                $(".loader").remove();
+                $("#accordion").removeClass("hidden");
+                $("p.resultHeader").html("Initial fetch complete!");
+                $(".allResults").append("You may now use the search bar!");
+            }
+        }
+    });
+    $("#personSearchForm .btn").click(function (e) {
+        e.preventDefault();
+        $("#personSearchForm input").removeClass("invalid");
+        var personCountry = countries[$("#personSearchForm input[name='personCountry']").val().split(" ").join("_")],
+            personProfession = professions[$("#personSearchForm input[name='personProfession']").val().split(" ").join("_")],
+            personName = $("#personSearchForm input[name='personName']").val(),
+            sendingData = {
+                personName: personName,
+                personProfession: personProfession,
+                personCountry: personCountry,
+            },
+            result;
+        if (personCountry !== undefined || personName.trim().length > 0 || personProfession !== undefined) {
+            
+            if (personProfession === undefined && $("#personSearchForm input[name='personProfession']").val().length > 0) {
+                $("#personSearchForm input[name='personProfession']").addClass("invalid");
+            }
+            
+            if (personCountry === undefined && $("#personSearchForm input[name='personCountry']").val().length > 0) {
+                $("#personSearchForm input[name='personCountry']").addClass("invalid");
+            }
+            
+            $("p.resultHeader").html("Fetching data.. please wait");
+            $(".allResults").html("");
+            jQuery.ajax({
+                method: 'get',
+                url: "search/people",
+                dataType: "json",
+                data: sendingData,
+                success: function (data) {
+                    console.log(data);
+                    var count = 0,
+                        title,
+                        result,
+                        prop;
+                    for (prop in data) {
+                        if (data.hasOwnProperty(prop)) {
+                            count += 1;
+                        }
+                    }
+                    $("p.resultHeader").html("There are " + count + " results based on your latest query.");
+                    $.each(data, function (key, val) {
+                        result = '<div class="col-lg-6 col-md-6 col-sm-12">' +
+                            '<div class="resultWrapper">' +
+                            '<p>Type: <span class="type">Person</span></p>' +
+                            '<p>Name: <span class="name">' + val.name + '</span></p>' +
+                            '<p>Profession: <span class="profession">' + val.profession.name + '</span></p>' +
+                            '<p>Country: <span class="profession">' + val.country.name + '</span></p>' +
+                            '<a target="_blank" href="' + val.link + '"> Original Link</a>' +
+                            '<button type="button" class="addToList"><span class="glyphicon glyphicon-plus"></span></button>' +
+                            '<button type="button" class="removeResult"><span class="glyphicon glyphicon-minus"></span></button></div></div>';
+                        $(".allResults").append(result);
+                    });
+                },
+                error: function (data) {
+                    $("p.resultHeader").html("Something wrong happened. Please try again.");
+                }
+            });
+        }
+    });
     $("#bookSearchForm .btn").click(function (e) {
         e.preventDefault();
         $("#bookSearchForm input").removeClass("invalid");
@@ -322,7 +421,7 @@ $(document).on('ready', function () {
                 numberOfVolumes: numberOfVolumes
             },
             result;
-        if (authorUri === undefined && titleName.trim().length === 0 || literaryGenreUri === undefined || illustratorUri === undefined || numberOfPagesMin.trim().length === 0 || numberOfPagesMax.trim().length === 0 || numberOfVolumes.trim().length === 0) {
+        if (authorUri === undefined || titleName.trim().length > 0 || literaryGenreUri === undefined || illustratorUri === undefined || numberOfPagesMin.trim().length > 0 || numberOfPagesMax.trim().length > 0 || numberOfVolumes.trim().length > 0) {
             
             if (illustratorUri === undefined && $("#bookSearchForm input[name='illustratorUri']").val().length > 0) {
                 $("#bookSearchForm input[name='illustratorUri']").addClass("invalid");
@@ -344,6 +443,7 @@ $(document).on('ready', function () {
                 dataType: "json",
                 data: sendingData,
                 success: function (data) {
+                    console.log(data);
                     var count = 0,
                         title,
                         result,
@@ -357,17 +457,23 @@ $(document).on('ready', function () {
                     $.each(data, function (key, val) {
                         result = '<div class="col-lg-6 col-md-6 col-sm-12">' +
                             '<div class="resultWrapper">' +
-                            '<p>Type: <span class="type">Place</span></p>' +
+                            '<p>Type: <span class="type">Book</span></p>' +
                             '<p>Name: <span class="name">' + val.title + '</span></p>';
-                            if(val.releaseDate) {
-                                if(typeof val.releaseDate === 'object') {
-                                    result += '<p>Name: <span class="releaseDate">' + val.releaseDate.date.split(" ")[0] + '</span></p>';
-                                }
-                                if(typeof val.releaseDate !== 'object') {
-                                    result += '<p>Year: <span class="releaseDate">' + val.releaseDate + '</span></p>';
-                                }
+                        
+                        if(val.authors) {
+                            $.each(val.authors, function (key2, val2) {
+                                result += '<p>Author: <span class="releaseDate">' + val2 + '</span></p>';
+                            });
+                        }
+                        if(val.releaseDate) {
+                            if(typeof val.releaseDate === 'object') {
+                                result += '<p>Name: <span class="releaseDate">' + val.releaseDate.date.split(" ")[0] + '</span></p>';
                             }
-                            result += '<a target="_blank" href="' + val.link + '"> Original Link</a>' +
+                            if(typeof val.releaseDate !== 'object') {
+                                result += '<p>Year: <span class="releaseDate">' + val.releaseDate + '</span></p>';
+                            }
+                        }
+                        result += '<a target="_blank" href="' + val.link + '"> Original Link</a>' +
                             '<button type="button" class="addToList"><span class="glyphicon glyphicon-plus"></span></button>' +
                             '<button type="button" class="removeResult"><span class="glyphicon glyphicon-minus"></span></button></div></div>';
                         $(".allResults").append(result);
@@ -379,7 +485,6 @@ $(document).on('ready', function () {
             });
         }
     });
-    
     $("#placeSearchForm .btn").click(function (e) {
         e.preventDefault();
         $("#placeSearchForm input").removeClass("invalid");
